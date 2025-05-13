e9123e0f6473b26e679ffa553e3493ec16dc189a
Harsh <khaitanharsh08@gmail.com>
2025-05-14 03:33:18 +0530

    emp done

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+class Employee:
+    def __init__(self):
+        print("the object creation starts")
+        self.id = 100
+        self.designation = "C1"
+        print("the objection creation ends")
+    def travel(self):
+        self.travell = "JAMMU"
+        print(f"The emloyee id: {self.id} is travelling to {self.travell}")
+harsh = Employee()
+        
